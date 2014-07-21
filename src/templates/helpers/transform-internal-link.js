265b0df3a57349b82c67cf254abd6c15ab06e315
Wanja Stier <Wanja.Stier@akqa.com>
2014-07-21 11:59:59 +0100

    no message

--- src/templates/helpers/transform-internal-link.js
+++ src/templates/helpers/transform-internal-link.js
@@ -3,10 +3,13 @@ module.exports.register = function (Handlebars) {
     'use strict';
 
 
-    /* chain Handlebars helpers together */
+    /**
+     * transform assemble destination link by removing the path to 'dist' and 'index.html'
+     */
     Handlebars.registerHelper('transformInternalLink', function (url) {
         var match = '/dist'
-        url = url.substring( url.indexOf(match) + match.length, url.length )
+        url = url.substring( url.indexOf(match) + match.length, url.length );
+        url = url.substring( 0, url.lastIndexOf('/'));
         return url;
     });
 }
